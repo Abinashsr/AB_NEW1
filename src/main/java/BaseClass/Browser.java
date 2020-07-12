033c1ba0fb991f592e8fc14acdf1ea335e918c9b
Abinash Smruti Ranjan <abinash.smruti.ranjan@ericsson.com>
2020-07-12 17:45:58 +0530

    13 commit

--- src/main/java/BaseClass/Browser.java
+++ src/main/java/BaseClass/Browser.java
@@ -2,6 +2,7 @@ package BaseClass;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
@@ -18,22 +19,31 @@ public class Browser {
 
 	public static WebDriver StartBrowser(String browsername, String url) throws InterruptedException {
 	
-		// If the browser is Chrome
-			// Set the path for chromedriver.exe
-			//String path = System.getProperty("user.dir");
-			//System.out.println(path); 
-			System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
-			ChromeOptions options = new ChromeOptions();
-	        options.addArguments("headless");
-	        options.addArguments("window-size=1200x600");
-	        driver = new ChromeDriver(options);
-	  //      driver.get("https://contentstack.built.io");
-	  //      driver.get("http://test-webapp.gl-project1.com/webapp/welcome.jsp");
-	        System.out.println("Test Case Started");
-			//driver.manage().deleteAllCookies();
-			Thread.sleep(1000);
-			//driver.get(url);
-			return driver;
+		//String path = System.getProperty("user.dir");
+		//System.out.println(path);
+		System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
+		HashMap<String, Object> chromePrefs = new HashMap<>();
+		chromePrefs.put("profile.default_content_settings.popups", 0);
+		//chromePrefs.put("download.default_directory", path);
+		ChromeOptions options = new ChromeOptions();
+		options.setExperimentalOption("prefs", chromePrefs);
+		       options.addArguments("--no-sandbox"); //Bypass OS security model
+		            options.addArguments("--disable-web-security");
+		            options.addArguments("--ignore-urlfetcher-cert-requests");
+		            options.addArguments("--disable-renderer-backgrounding");
+		            options.addArguments("--disable-infobars"); //Disabling infobars
+		            options.addArguments("--start-maximized"); //Open Browser in maximized mode
+		            options.addArguments("--disable-dev-shm-usage"); //Overcome limited resource problem
+		            options.setExperimentalOption("useAutomationExtension", false);
+		            options.addArguments("--log-level=3"); // set log level
+		            options.addArguments("--silent");
+		            options.addArguments("--disable-gpu");
+		            options.addArguments("window-size=1920,1080");
+		            options.addArguments("headless");
+		       driver = new ChromeDriver(options);
+		       System.out.println("Test Case Started");
+		       Thread.sleep(5000);
+		       return driver;
 	}
 	
 	
